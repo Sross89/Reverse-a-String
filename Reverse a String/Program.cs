b4e843284ccc26692cb0e338cf6d61a34d587df6
Simon Ross <simon.alexander.ross@gmail.com>
2020-01-29 08:56:35 -0700

    - Wrote the program

---
+++ Reverse a String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool appIsRunning = true;
+            while (appIsRunning = true)
+            {
+                Console.WriteLine("Enter a string: ");
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Reversed string: ");
+                Console.WriteLine(ReverseString(input));
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        static string ReverseString(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                string msg = "No string entered";
+                return msg;
+            }
+            string output = "";
+            for (int i = input.Length; i > 0; i--)
+            {
+                output += input[i-1];
+            }
+            return output;
+        }
+    }
+}
